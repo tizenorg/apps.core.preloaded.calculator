dcf42e5e4974ac0b32560c967d1bbced39b7a5c9
Philippe Coval <philippe.coval@open.eurogiciel.org>
2014-03-21 10:41:09 +0100

    also support efl-1.7 with tizen patches
    Change may be skipped once efl-1.7 is no more supported in tizen
    
    Bug-Tizen: TIVI-2726/part
    Change-Id: Ia3b1aa5ea1b43e91b4be21f65ac42c5e40efdf7c
    Signed-off-by: Philippe Coval <philippe.coval@open.eurogiciel.org>

--- src/calc-view.c
+++ src/calc-view.c
@@ -459,7 +459,11 @@ _calc_view_create_history_entry(Evas_Object * parent)
 	elm_entry_single_line_set(entry, EINA_FALSE);
 	elm_entry_line_wrap_set(entry, ELM_WRAP_WORD);
 	elm_entry_editable_set(entry, EINA_FALSE);
+#if (ELM_VERSION_MAJOR == 1) && (ELM_VERSION_MINOR < 8)
+	elm_entry_magnifier_disabled_set(entry, EINA_TRUE);
+#else
 	elm_config_magnifier_enable_set(EINA_FALSE);
+#endif
 	elm_entry_entry_set(entry, "<align=right>");
 	evas_object_size_hint_weight_set(entry, EVAS_HINT_EXPAND, 0.0);
 	evas_object_size_hint_align_set(entry, EVAS_HINT_FILL, 0.0);
